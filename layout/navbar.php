a9ee6c58b29a57493f9a5f4c8c4442c8ffc2093a
Mimachh <karl.mullr@gmail.com>
2022-10-15 16:17:07 +0200

    ajout d'animations sur la page membres + couleur des cards

--- layout/navbar.php
+++ layout/navbar.php
@@ -7,6 +7,8 @@
     <title>A-Sauce</title>
     <link rel="stylesheet" href="./sass/bootstrap.css">
     <link rel="stylesheet" href="./Style/style.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
 </head>
   <body class="bg-dark">
     <header>
--- membres.php
+++ membres.php
@@ -2,20 +2,21 @@
     
 
     <main>
-
-      <div class="container-fluid text-white-50">
-        <h1>Nos différents membres :</h1>
+      <!-- Menu pour trouver les membres -->
+      <div class="container-fluid text-white-50 pt-4">
+        <h1 class=" animate__animated animate__bounceInLeft">Nos différents membres :</h1>
         <ol id="menu">
-          <li><a href="#frodon">Fraudons et Sam</a></li>
-          <li><a href="#aragorn">Aragorn, Légolas, Gimli</a></li>
-          <li><a href="#gandalf">Glandalf</a></li>
-          <li><a href="">Bor-au-mir</a></li>
-          <li><a href="">Mer-riz et Pépin</a></li>
+          <li><a class="member_move" href="#frodon">Fraudons et Sam</a></li>
+          <li><a class="member_move" href="#aragorn">Aragorn, Légolas, Gimli</a></li>
+          <li><a class="member_move" href="#gandalf">Glandalf</a></li>
+          <li><a class="member_move" href="#boromir">Bor-au-mir</a></li>
+          <li><a class="member_move" href="">Mer-riz et Pépin</a></li>
       </ol>  
       </div>
+        <HR class="mx-auto">
 
         <!-- Team 1-->
-        <div class="text-white-50 container-fluid mt-4 team1 rounded-3" id="frodon">
+        <div class="text-dark container-fluid mt-4 team1 rounded-3 animate__animated animate__backInRight" id="frodon">
 
           <div class="row ">
               
@@ -24,15 +25,16 @@
               <div class="col-12 col-sm-12 col-md-5 p-4 ">
                   <h1 class="text-center pb-4">Fraudons et Sam</h1>
                   <h3 class="pb-2">Nos 2 compagnons issus de la gauche écologiste. </h3>
-                  <p>Attachants, faciles à vivre. Sam(à gauche) est très bon cuisinier, Fraudons (à droite) est un un opportuniste ayant passé sa vie en viager avec son oncle. <br>
+                  <p>Attachants, faciles à vivre. Sam (à gauche) est très bon cuisinier, Fraudons (à droite) est un opportuniste ayant passé sa vie en viager avec son oncle. <br>
                   Par le biais de cette association, ils auront appris à se connaitre et s'apprécier sous un jour nouveau. </p>
               </div>
           </div>
         </div>
-        
+        <HR class="mx-auto">
+
         <!-- Team 2-->
 
-          <div class="text-white-50 container-fluid" id="aragorn">
+          <div class="text-white-50 container-fluid team2 rounded-3 animate__animated animate__backInLeft" id="aragorn">
 
             <div class="row  inverse-order">
                 <div class="col-12 col-sm-12 col-md-5 p-4">
@@ -42,7 +44,7 @@
                     <ul>
                       <li>Aragorn (à gauche) est un descendant <a target="_blank" href="https://gw.geneanet.org/tolkien1?n=of+the+dunedains&oc=2&p=aragorn&type=tree">d'une fière lignée</a>, en communiste qu'il est, il refusa pendant de longues années cet héritage
                   préférant vivre chez son beau-père Elrond.</li> 
-                      <li>Legolas (au centre) parcours le monde depuis qu'il a été chassé de la forêt de son père. </li> 
+                      <li>Legolas (au centre), éternel tanguy, parcours le monde depuis qu'il a été chassé de la forêt de son père. </li> 
                       <li>Enfin, Gimli, à droite. Besogneux, s'adonne à la nouvelle mode du lancer de hache dont il 
                         est vice-champion du monde <a href="https://lescognees.com/technique-lancer-haches/" target="_blank">(en prise marteau)</a> .
                       </li>
@@ -56,9 +58,10 @@
               
             </div>
           </div>
+        <HR class="mx-auto">
 
           <!-- Team 3-->
-          <div class="text-white-50 container-fluid" id="gandalf">
+          <div class="text-white-50 container-fluid team3 rounded-3 animate__animated animate__backInRight" id="gandalf">
 
             <div class="row">
 
@@ -68,15 +71,16 @@
                 <h1 class="text-center pb-4">Glandalf</h1>
                 <h3 class="pb-2">L'hermite </h3>
                 <p>Un sans adresse-fixe qui a roulé sa bosse, fumeur de pipe, spécialiste des feux d'artifice.<br> 
-                  Compte se reconvertir en concepteur de pont lorsqu'il aura quitté la communauté. </p>
+                  Compte se reconvertir en concepteur de pont suite à une mauvaise expérience. </p>
               </div>
             </div>
           </div>
 
+        <HR class="mx-auto">
 
           <!-- Team 4-->
 
-          <div class="text-white-50 container-fluid">
+          <div class="text-white-50 container-fluid team4 rounded-3 animate__animated animate__backInLeft" id="boromir">
 
             <div class="row inverse-order">
               <div class="col-12 col-sm-12 col-md-5 p-4">
@@ -87,7 +91,7 @@
               </div>
             
                 
-            <img src="./Photos/boromir.jpg" alt="Photo de Boromir" class="col-12 col-sm-12 col-md-7 p-4 rounded-5 photo-start">
+              <img src="./Photos/boromir.jpg" alt="Photo de Boromir" class="col-12 col-sm-12 col-md-7 p-4 rounded-5 photo-start">
   
               
             </div> 
